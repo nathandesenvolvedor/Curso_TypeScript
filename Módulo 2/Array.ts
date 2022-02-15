42c9a0bae41a8353033ff7e7ef12baab43c4f375
nathandesenvolvedor <85134701+nathandesenvolvedor@users.noreply.github.com>
2022-02-15 17:23:34 -0300

    módulo 2

---
+++ Módulo 2/Array.ts
@@ -0,0 +1,29 @@
+// ==> Exemplo 1 - Uso do Colchetes
+let frutas: string[] = ['Maça', 'Laranja', 'Limão', 'Abacaxi'];
+
+console.log(frutas); //Maça,Laranja,Limão,Abacaxi
+
+// ==> Exemplo 2 - Uso do Colchetes
+let frutas2: string[] = ['Maça', 'Laranja', 'Limão', 'Abacaxi']
+
+console.log(frutas2[1]);//Laranja
+
+// ==> Exemplo 3 - Array object (object Array)
+let frutas3: Array<string> = ['Maça', 'Laranja', 'Limão', 'Abacaxi']
+
+console.log(frutas2[0]);//Maça
+
+// ==> Exemplo 4 - adicionando mais elementos no Array com "push"
+let frutas4: Array<string> = ['Maça', 'Laranja', 'Limão', 'Abacaxi']
+frutas4.push('Uva');//Acrescenta = [ 'Maça', 'Laranja', 'Limão', 'Abacaxi', 'Uva' ]
+console.log(frutas4);
+
+// ==> Exemplo 5 - Indentificar o tamanho do Array, método 'length' 
+let frutas5: Array<string> = ['Maça', 'Laranja', 'Limão', 'Abacaxi']
+
+console.log(frutas5.length);//4
+
+// ==> Exemplo 6 - Array com Spread Operator 
+let listNumber: Array<number> = [1, 2, 3, 4, 5, 6]
+listNumber = [...listNumber, 7, 8, 9, 10]//A continuação do Array
+console.log(listNumber);//1,2,3,4,5,6,7,8,9,10
