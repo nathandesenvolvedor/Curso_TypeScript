e0d67a1c014718ea431fc6f67e311dcf10fdcbaf
nathandesenvolvedor <85134701+nathandesenvolvedor@users.noreply.github.com>
2022-02-15 15:54:03 -0300

    configm

---
+++ Módulo 1/DeclaraçãoDeVariaveis.ts
@@ -0,0 +1,12 @@
+//No TypeScript temos que colocar o tipo de variavel, como:
+//==>Boolean;
+//==>String;
+//==>Number;
+//Já no ECMAScript6 (do JavaScript) podemos utilizar concatenar utilizando o templete string.
+
+let cliente: string = 'Nathan Vitor';
+let confirmacaoDaCompra: boolean = true;
+let valorASerPago: number = 1000;
+console.log(cliente);
+console.log(confirmacaoDaCompra);
+console.log(valorASerPago);
